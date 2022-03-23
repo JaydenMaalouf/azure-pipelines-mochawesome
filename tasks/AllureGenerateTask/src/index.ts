443804531cb8998e9c3dfa057341c165a42d5606
JaydenMaalouf <jayden.m.maalouf@gmail.com>
2022-03-23 23:41:33 +1100

    feat: :art: renamed the installer task

--- tasks/AllureGenerateTask/src/index.ts
+++ tasks/AllureGenerateTask/src/index.ts
@@ -1,6 +1,9 @@
 import task = require("azure-pipelines-task-lib/task");
+import fs = require("fs");
+import path = require("path");
 import { TaskResult } from "azure-pipelines-task-lib/task";
 import { IExecOptions } from "azure-pipelines-task-lib/toolrunner";
+import { stringify } from "querystring";
 
 async function run() {
   try {
@@ -12,20 +15,43 @@ async function run() {
     }
 
     let allureTool = task.tool(allurePath);
-    allureTool.arg(["generate", "-o", task.getInput("outputDirectory")]);
+    let outputDirectory = task.getInput("outputDirectory");
+    allureTool.arg(["generate"]);
+    allureTool.argIf(outputDirectory, ["-o", outputDirectory]);
 
+    let workingDirectory = task.getInput("workingDirectory");
     let result = await allureTool.exec(<IExecOptions>{
-      cwd: task.getInput("workingDirectory"),
+      cwd: workingDirectory,
     });
-    if (result == 0) {
-      task.setResult(TaskResult.Succeeded, "");
-      return;
+    if (result != 0) {
+      throw `Allure Generation failed with Exit Code: ${result}`;
     }
 
-    throw `Allure Generation failed with Exit Code: ${result}`;
+    if (outputDirectory) {
+      let files = getAllFiles(outputDirectory);
+      files.forEach((file) => {
+        task.addAttachment("allure.report", "output", file);
+      });
+    }
   } catch (error) {
     task.setResult(task.TaskResult.Failed, error);
   }
 }
+
+function getAllFiles (dirPath: string, arrayOfFiles?: string[]) {
+  let files = fs.readdirSync(dirPath);
+
+  arrayOfFiles = arrayOfFiles || [];
+
+  files.forEach(function (file) {
+    if (fs.statSync(dirPath + "/" + file).isDirectory()) {
+      arrayOfFiles = getAllFiles(dirPath + "/" + file, arrayOfFiles);
+    } else {
+      arrayOfFiles.push(path.join(__dirname, dirPath, "/", file));
+    }
+  });
+
+  return arrayOfFiles;
+};
 
 run();
